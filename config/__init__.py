4023d8aba3fe0806ff1c71480fd58f20aac76ba8
Julio Ubeda Quesada <julioubedaquesada@gmail.com>
2026-03-02 09:35:41 +0100

    Dejar de trackear config

--- config/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Definir la ruta al archivo JSON dentro del paquete `config`
-config_path_exp = os.path.join(os.path.dirname(__file__), "./conf_global/app-config-exp.json")
-config_path_prod = os.path.join(os.path.dirname(__file__), "./conf_global/app-config-prod.json")
-config_path_fempa = os.path.join(os.path.dirname(__file__), "./conf_global/app-config-fempa.json")
-config_path_server = os.path.join(os.path.dirname(__file__), "./conf_global/server-config.json")
